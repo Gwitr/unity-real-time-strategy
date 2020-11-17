bf3f3567d1124f5b0826392c12d7c9d52f47fce9
Robowiko123 <robowiko@gmail.com>
2020-11-17 11:10:02 +0100

    Add fault handler for invalid auth in 'ZAPISZ PROJEKT NA SERWER.py'

--- ZAPISZ PROJEKT NA SERWER.py
+++ ZAPISZ PROJEKT NA SERWER.py
@@ -7,10 +7,16 @@ def cmd(c):
     r = os.system(c)
     if r != 0:
         print("error: '%s' returned code %d" % (c, r))
+        input("Naciśnij Enter, aby kontynuować . . .")
         sys.exit(1)
 
 commit_msg = input("Wiadomość: ")
 cmd("git add .")
 cmd("git commit -am \"%s\"" % (commit_msg.replace("\\", "\\\\").replace("\"", "\\\"")))
-cmd("git push")
+# cmd("git push")
+r = os.system("git push")
+while r == 128:
+    print("ERROR: Niepoprawna nazwa użytkownika lub hasło.")
+    r = os.system("git push")
+
 input("Naciśnij Enter, aby kontynuować . . .")
